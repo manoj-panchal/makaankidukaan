955ea2c3b6b2b6c510147cc57f17783c07cf2652
manoj-panchal <manoj.panchal@kiwitech.com>
2018-05-20 12:07:11 +0530

    unnecessary code

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -9,13 +9,13 @@ $("#searchLocation").on('click',function() {
     var options = {
       map: "#map_canvas",
       location: 'India'
-      mapOptions: {
-       streetViewControl: false,
-       mapTypeId : google.maps.MapTypeId.HYBRID
-      },
-      markerOptions: {
-        draggable: true
-      }
+     // mapOptions: {
+     //  streetViewControl: false,
+     //  mapTypeId : google.maps.MapTypeId.HYBRID
+     // },
+      //markerOptions: {
+       // draggable: true
+      //}
     };
 	$('#searchLocation').geocomplete().bind("geocode:result", function(event, result){
       var map = $(this).geocomplete("map");
